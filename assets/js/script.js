81668feb797cc3de59d2ca7a5517576d9ca62c43
LeJuanTenorio <tenorio2003@gmail.com>
2023-05-20 20:28:43 -0500

    :)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,33 @@
-const textbox = document.querySelector("textbox")
+const textbox = document.querySelector(".textbox");
 
+let keyNum = 1;
+
+const stringifyThis = (x) =>{
+    JSON.stringify(x);
+}
+
+const parseThis = (x) => {
+    JSON.parse(x);
+}
+
+const storeLocally = (event) => {
+  event.preventDefault();
+
+  const input = document.querySelector(".writtenTask");
+
+  const status = "To do"
+  
+  keyNum++;
+
+  localStorage.setItem(input.value, status);
+
+   
+};
+
+const showList = () => {
+  const retrieveLocalStorage = Object.keys(localStorage);
+  console.log(retrieveLocalStorage);
+};
+
+textbox.addEventListener("submit", storeLocally);
+textbox.addEventListener("submit", showList);
